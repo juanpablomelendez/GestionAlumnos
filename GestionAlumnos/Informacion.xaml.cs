7c74d9ee21ce347df4474778f670a8166b1d542e
juanpablomelendez <111584229+juanpablomelendez@users.noreply.github.com>
2023-10-29 14:50:54 -0600

    Add project files.

---
+++ GestionAlumnos/Informacion.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GestionAlumnos
+{
+    /// <summary>
+    /// Lógica de interacción para Informacion.xaml
+    /// </summary>
+    public partial class Informacion : Window
+    {
+        private SqlConnection miConexionSql;
+
+        public Informacion()
+        {
+            InitializeComponent();
+            /// GestionAlumnosConnectionString
+            InitializeComponent();
+            string miConexion = ConfigurationManager.ConnectionStrings["GestionAlumnos.Properties.Settings.GestionAlumnosConnectionString"].ConnectionString;
+            miConexionSql = new SqlConnection(miConexion);
+
+            MuestraAlumnos();
+            MuestraMaestros();
+            MuestraCursos();
+            MuestraAulas();
+            MuestraAsignaturas();
+            MuestraAlumnosCursos();
+            MuestraCursosAsignaturas();
+        }
+
+        private void MuestraAlumnos()
+        {
+            string consulta = "SELECT Matricula, CONCAT(Nombre, ' ', Apellido) AS NombreCompleto FROM Alumnos"; // Consulta para obtener Matricula e 'Nombre Apellido'
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
+
+            using (miAdaptadorSql)
+            {
+                DataTable alumnosTabla = new DataTable();
+                miAdaptadorSql.Fill(alumnosTabla);
+
+                // Configura la lista para mostrar el ID y el nombre completo
+                ListaAlumnos.DisplayMemberPath = "NombreCompleto";
+                ListaAlumnos.SelectedValuePath = "Matricula";
+                ListaAlumnos.ItemsSource = alumnosTabla.DefaultView;
+            }
+        }
+
+        private void MuestraMaestros()
+        {
+            string consulta = "SELECT MaestroID, CONCAT(Nombre, ' ', Apellido) AS NombreCompleto FROM Maestros";
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
+
+            using (miAdaptadorSql)
+            {
+                DataTable maestrosTabla = new DataTable();
+                miAdaptadorSql.Fill(maestrosTabla);
+
+                listaMaestros.DisplayMemberPath = "NombreCompleto";
+                listaMaestros.SelectedValuePath = "MaestroID";
+                listaMaestros.ItemsSource = maestrosTabla.DefaultView;
+            }
+        }
+
+        private void MuestraCursos()
+        {
+            string consulta = "SELECT CursoID, NombreCurso FROM Cursos";
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
+
+            using (miAdaptadorSql)
+            {
+                DataTable cursosTabla = new DataTable();
+                miAdaptadorSql.Fill(cursosTabla);
+
+                ListaCursos.DisplayMemberPath = "NombreCurso";
+                ListaCursos.SelectedValuePath = "CursoID";
+                ListaCursos.ItemsSource = cursosTabla.DefaultView;
+            }
+        }
+
+        private void MuestraAulas()
+        {
+            string consulta = "SELECT AulaID, NombreAula FROM Aulas";
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
+
+            using (miAdaptadorSql)
+            {
+                DataTable aulasTabla = new DataTable();
+                miAdaptadorSql.Fill(aulasTabla);
+
+                ListaAulas.DisplayMemberPath = "NombreAula";
+                ListaAulas.SelectedValuePath = "AulaID";
+                ListaAulas.ItemsSource = aulasTabla.DefaultView;
+            }
+        }
+
+        private void MuestraAsignaturas()
+        {
+            string consulta = "SELECT AsignaturaID, NombreAsignatura FROM Asignaturas";
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
+
+            using (miAdaptadorSql)
+            {
+                DataTable asignaturasTabla = new DataTable();
+                miAdaptadorSql.Fill(asignaturasTabla);
+
+                ListaAsignaturas.DisplayMemberPath = "NombreAsignatura";
+                ListaAsignaturas.SelectedValuePath = "AsignaturaID";
+                ListaAsignaturas.ItemsSource = asignaturasTabla.DefaultView;
+            }
+        }
+
+        private void MuestraAlumnosCursos()
+        {
+            string consulta = "SELECT Matricula, CursoID FROM AlumnosCursos";
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
+
+            using (miAdaptadorSql)
+            {
+                DataTable alumnosCursosTabla = new DataTable();
+                miAdaptadorSql.Fill(alumnosCursosTabla);
+
+                ListaAlumnosCursos.DisplayMemberPath = "CursoID"; // Puedes mostrar la información que desees aquí
+                ListaAlumnosCursos.SelectedValuePath = "Matricula"; // Puedes configurar esto según tus necesidades
+                ListaAlumnosCursos.ItemsSource = alumnosCursosTabla.DefaultView;
+            }
+        }
+
+        private void MuestraCursosAsignaturas()
+        {
+            string consulta = "SELECT CursoID, AsignaturaID FROM CursosAsignaturas";
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
+
+            using (miAdaptadorSql)
+            {
+                DataTable cursosAsignaturasTabla = new DataTable();
+                miAdaptadorSql.Fill(cursosAsignaturasTabla);
+
+                ListaCursosAsignaturas.DisplayMemberPath = "AsignaturaID"; // Puedes mostrar la información que desees aquí
+                ListaCursosAsignaturas.SelectedValuePath = "CursoID"; // Puedes configurar esto según tus necesidades
+                ListaCursosAsignaturas.ItemsSource = cursosAsignaturasTabla.DefaultView;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ GestionAlumnos/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace GestionAlumnos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            /// GestionAlumnosConnectionString
+            InitializeComponent();
+            string miConexion = ConfigurationManager.ConnectionStrings["GestionAlumnos.Properties.Settings.GestionAlumnosConnectionString"].ConnectionString;
+            //miConexionSql = new SqlConnection(miConexion);
+        }
+
+        private void contraseña_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            string correoIngresado = correo.Text; // Supongamos que 'correo' es el TextBox para el correo.
+            string contraseñaIngresada = contrasena.Text; // Supongamos que 'contraseña' es el TextBox para la contraseña.
+
+            if (ValidarCredenciales(correoIngresado, contraseñaIngresada))
+            {
+                // Credenciales válidas, abre la segunda ventana.
+                Window1 ventana2 = new Window1();
+                ventana2.Show();
+                // Cierra la ventana actual si es necesario.
+                this.Close();
+            }
+            else
+            {
+                // Credenciales incorrectas, muestra un mensaje de error.
+                MessageBox.Show("Credenciales incorrectas. Por favor, verifica tu correo y contraseña.");
+            }
+        }
+        private bool ValidarCredenciales(string correo, string contraseña)
+        {
+            // Define las credenciales válidas
+            string correoValido = "juanpabloxz@outlook.com";
+            string contraseñaValida = "12345";
+
+            // Compara las credenciales ingresadas con las credenciales válidas
+            return correo == correoValido && contraseña == contraseñaValida;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            // Obtiene la contraseña ingresada por el usuario
+            string contraseñaIngresada = contrasena.Text;
+
+            // Verifica si la contraseña ingresada es la correcta (en este caso, "1234")
+            if (ValidarCredenciales(contraseñaIngresada))
+            {
+                // Contraseña correcta, abre la ventana "Informacion"
+                Informacion info = new Informacion();
+                info.Show();
+            }
+            else
+            {
+                // Contraseña incorrecta, muestra un mensaje
+                MessageBox.Show("Contraseña incorrecta. Inténtelo de nuevo.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private bool ValidarCredenciales(string contraseñaIngresada)
+        {
+            // Aquí puedes implementar la lógica de verificación de la contraseña
+            // Por ejemplo, puedes comparar la contraseña ingresada con la contraseña correcta
+            return contraseñaIngresada == "1234";
+        }
+
+
+        private void correo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void salir_Click(object sender, RoutedEventArgs e)
+        {
+             this.Close();
+        }
+    }
+}
